596d4ac9097595596bfb338c1d33c3afd231b8ed
Simeon Borisov <centa2010@gmail.com>
2021-06-11 15:08:44 +0300

    Madlib Game

---
+++ Madlibs.py
@@ -0,0 +1,13 @@
+# youtuber = "Pewdiepie"
+#
+# print("subscribe to:" + youtuber)
+# print(f'subscribe to {youtuber}')
+
+adj = input("Adjective: ")
+verb1 = input("Verb1: ")
+verb2 = input("Verb2: ")
+famous_person = input("Famous person: ")
+madlib = f'Computer programming is so {adj}! It makes me so excited all the time because\
+            i love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!'
+
+print(madlib)
